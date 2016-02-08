6e5a679d79e3c9c3d5642bad6a685c563e235a1f
Michael Washburn <washburn.michael@gmail.com>
2016-02-08 09:02:11 -0800

    added post filtering and function to add new title, but not working

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var app = angular.module('travelApp', [])
-  .controller('MainController', MainController)
+  .controller('MainController', MainController);
+
+  MainController.$inject = ['$http'];
 
   function MainController(){
-    var self = this
+    var self = this;
 
-    this.test = "MainController working"
+    self.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 4},
+    {title: 'post 3', upvotes: 2}
+    ],
+
+    self.addpost = function (){
+    self.posts.push({title: self.title, upvotes: 0});
+    self.title = '';
   }
+  }
+
+
