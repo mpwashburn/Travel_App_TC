39fe147ffa16cad97c72e6665d3388ce2f31ead3
Michael Washburn <washburn.michael@gmail.com>
2016-02-16 18:09:02 -0800

    working on bug to display usercontent

--- front_end_angular/js/contentCntrl.js
+++ front_end_angular/js/contentCntrl.js
@@ -4,7 +4,7 @@ function contentCntrl(){
   var self = this
 
   self.content = [
-      {src_url: 'http:"//postcardandmenu.com/2015/01/grand-central-market-los-angeles/"', image: 'assets/images/grand-central-market-los-angeles', title: 'Grand Central Market Los Angeles', body: 'The Grand Central Market is nearly a century old, but their commitment to bringing communities around a shared table has stayed the same. This historic Downtown Los Angeles landmark... ', lat_long: float},
-      {src_url: url, image: url, title: text, body: text, lat_long: float}
+      {src_url: 'http:"//postcardandmenu.com/2015/01/grand-central-market-los-angeles/"', image: 'assets/images/grand-central-market-los-angeles', title: 'Grand Central Market Los Angeles', body: 'The Grand Central Market is nearly a century old, but their commitment to bringing communities around a shared table has stayed the same. This historic Downtown Los Angeles landmark... '},
+      {src_url: url, image: url, title: text, body: text}
   ]
 }
--- front_end_angular/js/usersCntrl.js
+++ front_end_angular/js/usersCntrl.js
@@ -7,15 +7,18 @@ function usersCntrl(){
     {f_name: "Michael", l_name: "Washburn", email: "washburn.michael@gmail.com", username: "mpwashburn", password: "123456"},
     {f_name:"Loretta", l_name: "Jones", email: "lorettajones@gmail.com", username: "", password: "123456"}
   ];
-
-  self.addUser = addUser;
+  self.addUser    = addUser;
   self.deleteUser = deleteUser;
   self.updateUser = updateUser;
 
-// adds a user to userBase
+// adds a user to usersBase
   function addUser(){
     self.usersBase.push({f_name: self.text, l_name: self.text, email: self.text, username: self.text, password: self.text});
     self.text = null;
+  }
+
+  function update(user){
+    self.usersBase = angular.copy(user)
   }
 
   function deleteUser($index){
